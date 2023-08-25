6574b404265913327b9ed40e083ff1b62148aa0a
tugalsan karabacak <tugalsan@gmail.com>
2023-08-25 16:01:43 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/file/yaml/server/TS_FileYamlUtils.java
@@ -0,0 +1,7 @@
+package com.tugalsan.api.file.yaml.server;
+
+public class TS_FileYamlUtils {
+
+    final private static TS_Log d = TS_Log.of(TS_FileYamlUtils.class);
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.tugalsan.api.file.yaml {
+    requires com.tugalsan.api.log;
+    requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.file;
+    requires com.tugalsan.api.list;
+    requires com.tugalsan.api.stream;
+    requires com.tugalsan.api.string;
+    exports com.tugalsan.api.file.yaml.server;
+}
